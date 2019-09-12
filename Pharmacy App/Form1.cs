3aaf1c42e8453bb88381af929e9ce96798a05b37
Dylan Rossi <101624317@student.swin.edu.au>
2019-09-12 15:09:11 +1000

    Added project again

---
+++ Pharmacy App/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pharmacy_App
+{
+    public partial class Form1 : Form
+    {
+        private DatabaseContext context;
+        public Form1()
+        {
+            InitializeComponent();
+            context = new DatabaseContext();
+            Console.WriteLine(context.GetAllSales()[0].Product);
+            Console.WriteLine("hi");
+        }
+
+        private void ProgramBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
