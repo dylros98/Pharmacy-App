5a038aeb5338a785c4487adee828c2f468c10a18
dylros98 <101624317@student.swin.edu.au>
2019-09-13 20:50:06 +1000

    Added database context files and database record objects to existing form project from master with readme on how to setup database

---
+++ PharmacyApp/ProductRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp
+{
+    /// <summary>
+    /// Product record object that represents a product from the database
+    /// </summary>
+    public class ProductRecord
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Double { get; set; }
+
+        public string Category { get; set; }
+    }
+}
