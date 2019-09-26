26c09eb9e76347c79a94f7ffdbf706d849209a5f
Natalie <nataliesholakis@gmail.com>
2019-09-26 15:25:18 +1000

    Adding sales report generator

--- PharmacyApp/Report.cs
+++ PharmacyApp/Report.cs
@@ -12,9 +12,23 @@ namespace PharmacyApp
 {
     public partial class Report : Form
     {
+        private int rowIndex = 1;
         public Report()
         {
             InitializeComponent();
+        }
+
+        private void WeeklyReport_Load(object sender, EventArgs e)
+        {
+           /* foreach ( s in )
+            {
+                tlpDataRecords.Controls.Add(new Label() { Text = s.SaleID.ToString() }, 0, rowIndex);
+                tlpDataRecords.Controls.Add(new Label() { Text = s.Product.ToString() }, 1, rowIndex);
+                tlpDataRecords.Controls.Add(new Label() { Text = s.DateSold.ToString() }, 2, rowIndex);
+                tlpDataRecords.Controls.Add(new Label() { Text = s.Quantity.ToString() }, 3, rowIndex);
+                tlpDataRecords.Controls.Add(new Label() { Text = s.Price.ToString() }, 4, rowIndex);
+                rowIndex++;
+            }*/
         }
     }
 }
