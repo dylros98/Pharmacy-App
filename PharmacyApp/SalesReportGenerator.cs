22810535c11b9417ad472ef20ca898e0f6502eb0
ReefCole <46836237+ReefCole@users.noreply.github.com>
2019-09-26 15:35:01 +1000

    Sales Report Generator

--- PharmacyApp/SalesReportGenerator.cs
+++ PharmacyApp/SalesReportGenerator.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace PharmacyApp
 {
+    private DatabaseContext db;
+
     class SalesReportGenerator
     {
+        db = new DatabaseContext();
+    }
+
+    private List<SalesReport> ReportList(){
+        List<ProductRecord> products = db.GenerateallProduct();
+        int[] sales = db.GenerateSalesAmounts();
+        List<SalesReport> report = new SalesReport();
+        int i = 0;
+
+        foreach (ProductRecord record in products)
+        {
+            report.add(new SalesReport(){
+                ProductID = record.ProductID;
+                Name = record.Name;
+                Quantity = sales[i];
+                Price = record.Price;
+                Total = Quanitity * Price;
+            });
+        }
+
+        return report;
 
     }
 }
