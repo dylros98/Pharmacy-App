85cd975574883a1ba95f51b1fb7ce44f9009e377
ReefCole <7649967@student.swin.edu.au>
2019-10-17 03:22:58 +1100

    csvReportGenerator

---
+++ PharmacyApp/CsvReportGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using CsvHelper;
+
+namespace PharmacyApp
+{
+
+    class CsvReportGenerator
+    {
+
+            private List<SalesReport> Reports;
+
+            public void GenerateCsv() {
+
+            using (var stream = new StreamWriter("PharmacyApp\\Reports\\Report.csv"))
+            using (var csv = new CsvWriter(stream))
+            {
+                csv.WriteRecords(Reports);
+            }
+            }
+
+            public void OpenCsv() {
+                using (var reader = new StreamReader("PharmacyApp\\Reports\\Report.csv"))
+                {
+                }
+            }
+    }
+}
